225afc463e9f90bad8e5f810051f62647a035475
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-08 13:55:27 +0530

    upd

--- dags/test.py
+++ dags/test.py
@@ -1,12 +1,12 @@
 import datetime
 
-from airflow import DAG
+from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
 
-with DAG(
-    dag_id="Mayank_Test",
-    start_date=datetime.datetime(2025, 6, 8),
-    catchup=False,
-    schedule="@daily",
-):
-EmptyOperator(task_id="task")
+
+@dag(start_date=datetime.datetime(2025, 6, 8), catchup=False, schedule="@daily", tags=['testing'])
+def generate_dag():
+    EmptyOperator(task_id="task")
+
+
+generate_dag()

d69e1e38135f44e9a2efd34971e052c41547f4f3
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-08 12:50:11 +0530

    upd

--- dags/test.py
+++ dags/test.py
@@ -6,6 +6,7 @@ from airflow.operators.empty import EmptyOperator
 with DAG(
     dag_id="test",
     start_date=datetime.datetime(2025, 6, 8),
+    catchup=False,
     schedule="@daily",
 ):
 EmptyOperator(task_id="task")

647242f1e4003cdf76f7fc7f23236660b03a719f
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-08 12:50:59 +0530

    upd

--- dags/test.py
+++ dags/test.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="test",
+    dag_id="Mayank_Test",
     start_date=datetime.datetime(2025, 6, 8),
     catchup=False,
     schedule="@daily",

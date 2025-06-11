b41d566ff362dda246d086b3751b434bd6680f49
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-11 14:09:06 +0530

    upd

--- dags/stock_market_status.py
+++ dags/stock_market_status.py
@@ -1,27 +1,26 @@
 from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+from airflow.sensors.base import PokeReturnValue
 from datetime import datetime
+import requests
 
 
 @dag(
     start_date=datetime(2025,6,1),
     schedule='@daily',
     catchup=False,
-    tags=['stock_market_status']
+    tags=['stock_market']
 )
 
 def stock_market():
     
-    @task
-    def task_a():
-        print("Task A")
-        return 42
-
-    @task
-    def task_b(value):
-        print("Task B")
-        print(value)
-
-
-    task_b(task_a())
+    @task.sensor(poke_interval=5, timeout=None, mode='poke')
+    def is_api_available() -> PokeReturnValue:
+        api = BaseHook.get_connection('stock_api')
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        response = requests.get(url, headers=api.extra_dejson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done=condition)
 
 stock_market() 

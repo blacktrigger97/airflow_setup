b7ffd8736f0a8a6c5f9938291bb6bf0f3db006f9
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-24 04:32:18 +0530

    upd 2

--- dags/yfTicks.py
+++ dags/yfTicks.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 import os
 import sys
 from airflow.sdk import task, dag
-from airflow.providers.standard.operators.python import PythonOperator
+# from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime
 from chngdir import dir_chng
 
-sys.path.append(os.path.abspath('/root/airflow/jobs'))
+# sys.path.append(os.path.abspath('/root/airflow/jobs'))
 
-from jobs.pystrmMain import main
+# from pystrmMain import main
 
 @dag(schedule=None, start_date=datetime(2025, 12, 16), catchup=False)
 def install_and_use_module_dag():
@@ -28,11 +29,18 @@ def install_and_use_module_dag():
         print(f"pystrm version: {pystrm.__version__}")
         # ... your task logic here ...
 
-        fastInfo = PythonOperator(dag=dag,
+        # fastInfo = PythonOperator(dag=dag,
+        #         task_id='fastInfo',
+        #         provide_context=False,
+        #         python_callable=main,
+        #         op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
+        #     #    op_kwargs={'keyword_argument':'which will be passed to function'}
+        #         )
+
+        fastInfo = BashOperator(dag=dag,
                 task_id='fastInfo',
-                provide_context=False,
-                python_callable=main,
-                op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
+                bash_command="python pystrmMain.py liveYfinanaceTick Yfinance.FastInfo",
+                # op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
             #    op_kwargs={'keyword_argument':'which will be passed to function'}
                 )
         

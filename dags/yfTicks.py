5dff151cde49e372b28b162f0ca4dac88a8a2931
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-24 14:03:32 +0530

    upd 2

--- dags/yfTicks.py
+++ dags/yfTicks.py
@@ -12,43 +12,43 @@ def install_and_use_module_dag():
 
     dir_chng()
 
-    # fastInfo = PythonVirtualenvOperator(
-    #     task_id="fastInfo",
-    #     python_callable=main_function,
-    #     requirements=[
-    #         "pystrm",
-    #         "dill"
-    #     ],
-    #     system_site_packages=False,
-    #     inherit_env=True,
-    #     serializer="dill",
-    #     op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
-    # )
-
-    @task.virtualenv(
-        task_id="Ticks",
-        requirements=["pystrm"], # Specify packages and versions
-        system_site_packages=True, # Set to True to access system packages (including Airflow)
+    fastInfo = PythonVirtualenvOperator(
+        task_id="fastInfo",
+        python_callable=main_function,
+        requirements=[
+            "pystrm",
+            "dill"
+        ],
+        system_site_packages=False,
+        inherit_env=True,
+        serializer="dill",
+        op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
     )
-    def my_isolated_task():
-        # This code runs inside the new virtual environment
-        import pystrm
-        from pystrm import main_function
 
-        print(f"Python version in venv: {sys.version}")
-        print(f"pystrm version: {pystrm.__version__}")
-        # ... your task logic here ...
+    # @task.virtualenv(
+    #     task_id="Ticks",
+    #     requirements=["pystrm"], # Specify packages and versions
+    #     system_site_packages=True, # Set to True to access system packages (including Airflow)
+    # )
+    # def my_isolated_task():
+    #     # This code runs inside the new virtual environment
+    #     import pystrm
+    #     from pystrm import main_function
 
-        fastInfo = PythonVirtualenvOperator(dag=dag,
-                task_id='fastInfo',
-                provide_context=False,
-                python_callable=main_function,
-                op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
-            #    op_kwargs={'keyword_argument':'which will be passed to function'}
-                )
+    #     print(f"Python version in venv: {sys.version}")
+    #     print(f"pystrm version: {pystrm.__version__}")
+    #     # ... your task logic here ...
+
+    #     fastInfo = PythonVirtualenvOperator(dag=dag,
+    #             task_id='fastInfo',
+    #             provide_context=False,
+    #             python_callable=main_function,
+    #             op_args=['liveYfinanaceTick', 'Yfinance.FastInfo'],
+    #         #    op_kwargs={'keyword_argument':'which will be passed to function'}
+    #             )
         
-        fastInfo
+    fastInfo
 
-    my_isolated_task()
+    # my_isolated_task()
 
 install_and_use_module_dag()

a209d1505a6d790646e43986b60ca8680db847a8
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-27 14:49:29 +0530

    upd 2

--- dags/yfTicks.py
+++ dags/yfTicks.py
@@ -11,10 +11,10 @@ def install_and_use_module_dag():
     jobdir_chng()
 
     @task.virtualenv(
-        task_id="Ticks",
+        task_id="Ticks_FastInfo",
         system_site_packages=False, # Set to True to access system packages (including Airflow)
-        requirements=["pystrm"], # Specify packages and versions
-        inherit_env=True
+        requirements=["pystrm"] # Specify packages and versions
+        # inherit_env=True
     )
     def isolated_tick_task(mthd: str, key: str):
         # This code runs inside the new virtual environment

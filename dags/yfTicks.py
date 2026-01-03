09e7b7680c2f4aa2a5eb37d26a39cf9ec4ccd456
blacktrigger97 <blacktrigger97@gmail.com>
2026-01-03 21:59:35 +0530

    test upd 9

--- dags/yfTicks.py
+++ dags/yfTicks.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import sys
-import pandas_market_calendars as mcal
+# import pandas_market_calendars as mcal
 from datetime import datetime, date
 from time import sleep
 from utils import jobdir_chng
@@ -17,10 +17,12 @@ def install_and_use_module_dag():
     @task.virtualenv(
         task_id="mStatus",
         system_site_packages=False, # Set to True to access system packages (including Airflow)
-        requirements=["pystrm"], # Specify packages and versions
-        inherit_env=True
+        requirements=["pandas_market_calendars"], # Specify packages and versions
+        # inherit_env=True
     )
     def mStatus():
+
+        import pandas_market_calendars as mcal
 
         # Create the NSE calendar
         nse_calendar = mcal.get_calendar('XNSE')

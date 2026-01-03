9fdabf32adf44ef3b7f80ed1a14e93ae46728671
blacktrigger97 <blacktrigger97@gmail.com>
2026-01-03 21:58:25 +0530

    test upd 9

--- dags/yfTicks.py
+++ dags/yfTicks.py
@@ -14,7 +14,12 @@ def install_and_use_module_dag():
 
     jobdir_chng()
     
-    @task(task_id='mStatus')
+    @task.virtualenv(
+        task_id="mStatus",
+        system_site_packages=False, # Set to True to access system packages (including Airflow)
+        requirements=["pystrm"], # Specify packages and versions
+        inherit_env=True
+    )
     def mStatus():
 
         # Create the NSE calendar
@@ -79,9 +84,6 @@ def install_and_use_module_dag():
             trigger_next_run
 
 
-    runStatus = mStatus()
-    fastInfo = isolated_tick_task('liveYfinanaceTick', 'Yfinance.FastInfo')
-    nextDagRunCheck = reRunDag()
-    runStatus >> fastInfo >> nextDagRunCheck
+    mStatus() >> isolated_tick_task('liveYfinanaceTick', 'Yfinance.FastInfo') >> reRunDag()
     
 install_and_use_module_dag()

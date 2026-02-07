595dbe1ca74697425ed1461013c844f606d2c3fe
blacktrigger97 <blacktrigger97@gmail.com>
2026-02-07 13:42:06 +0530

    upd

---
+++ dags/3_operators.py
@@ -0,0 +1,28 @@
+from airflow.sdk import dag, task
+
+
+@dag(
+        dag_id='first_dag'
+)
+def first_dag():
+
+    @task.python
+    def first_task():
+        print("This is first task")
+
+    @task.python
+    def second_task():
+        print("This is second task")
+
+    @task.bash 
+    def run_after_loop():
+        return "echo https://airflow.apache.org/"
+
+    # Defining task dependencies
+    first = first_task()
+    second = second_task()
+    run_this = run_after_loop()
+
+    first >> second >> run_this
+
+first_dag()
